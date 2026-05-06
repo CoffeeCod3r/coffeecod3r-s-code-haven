e3d9e5d16b24b2e127985feb9b9d4ff31af99e0c
CoffeeCod3r <coffee.code.yt@gmail.com>
2026-05-06 11:53:33 +0200

    Update base path for custom domain coffeecod3r.dev

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/coffeecod3r-s-code-haven/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
